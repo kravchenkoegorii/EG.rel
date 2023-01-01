81e997182db6667d3e8742994a7fcf9132b7596f
Егор Кравченко <kravchenko13138@gmail.com>
2023-01-01 19:11:54 +0200

    Added migration

---
+++ EG.rel/Eg.rel.AuthService/Migrations/20230101162743_initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eg.rel.AuthService.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    passwordhash = table.Column<byte[]>(name: "password_hash", type: "bytea", nullable: false),
+                    passwordsalt = table.Column<byte[]>(name: "password_salt", type: "bytea", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
