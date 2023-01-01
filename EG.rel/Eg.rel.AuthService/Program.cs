32589ed702dc2589d136a6e9f5b3900e510f1df9
Егор Кравченко <kravchenko13138@gmail.com>
2023-01-01 15:01:51 +0200

    Added Eg.rel.AuthService

---
+++ EG.rel/Eg.rel.AuthService/Program.cs
@@ -0,0 +1,23 @@
+namespace Eg.rel.AuthService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<>();
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ EG.rel/Eg.rel.AuthService/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Eg.rel.AuthService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<WeatherDbContext>(options => options
+           .UseLazyLoadingProxies()
+           .EnableSensitiveDataLogging()
+           .UseNpgsql(Configuration.GetConnectionString("DefaultConnection"), o =>
+           {
+               o.UseNetTopologySuite();
+           })
+           .UseSnakeCaseNamingConvention());
+            services.AddSwaggerGen();
+            services.AddControllers();
+            services.AddCors();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger(options =>
+            {
+                options.RouteTemplate = "/swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EG.rel.WebApi v1");
+            }
+            );
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseCors(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
