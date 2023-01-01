206d7231730f5981195aceda50f0fbf39b3829ba
Егор Кравченко <kravchenko13138@gmail.com>
2023-01-01 18:04:27 +0200

    Added service extensions

---
+++ EG.rel/Eg.rel.AuthService/Services/ServicesExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Eg.rel.AuthService.Services
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorizationService, AuthorizationService>();
+            services.AddScoped<ITokenService, TokenService>();
+            return services;
+        }
+    }
+}
