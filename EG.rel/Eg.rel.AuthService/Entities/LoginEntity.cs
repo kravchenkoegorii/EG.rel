d6afacd2db44f3e7059c7b0a0b2ec5ff77d77664
Егор Кравченко <kravchenko13138@gmail.com>
2023-01-01 15:00:55 +0200

    Added login entity

---
+++ EG.rel/Eg.rel.AuthService/Entities/LoginEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Eg.rel.AuthService.Models
+{
+    public class LoginEntity
+    {
+        public Guid Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
